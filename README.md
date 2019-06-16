78ab3322dd825111395b22f73760064330a2b3ac
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2019-06-16 17:57:29 +0100

    Get thing publish-ready

--- README.md
+++ README.md
@@ -1,39 +1,28 @@
 # tmpdir
-Utility for writing tests with temporary directories
+Utility for writing tests with temporary directories;
 
-# Possible syntax
-## Wrap the function
+Ever in the situation that you need to work with real files in a test? If you're smart, you create those files in a temporary directory. This library manages the temporary directory for you by
+
+1. Returning a promise that contains the created temporary directory
+2. Cleaning up old temporary directories after a while, automatically.
+
+# Usage
 ```node
-describe('MyTest', () => withTmpDir((tmpDirPath) => {
-  myModule.thingThatHasSideEffects(
-    predefinedInputDir,
-    tmpDirPath
-  );
-  testOutputIsCorrect(tmpDirPath);
-}));
-```
-## Separate function
-```node
-describe('MyTest', () => {
-  tmpDirPath = getTmpDir();
-  myModule.thingThatHasSideEffects(
-    predefinedInputDir,
-    tmpDirPath
-  );
-  testOutputIsCorrect(tmpDirPath);
+tmpDir = require('tmpDir');
+
+tmpDir.provide().then((tempDir) => {
+  // this guarantees tempDir is a valid temporary directory
+  // tempDir is placed in the system's temporary directory
 });
-```
-... alternatively do this as a promise (oh man..)
-## beforeEach-style
-```node
-describe('MySuite', () => {
-  beforeEach(() => {
-    // create tmp dir here, clean up afterward, somehow pass the information to the main test
-  });
-});
-```
-TODO(cuontheinternet): Chai spies
-TODO(cuontheinternet): Decorator
 
-require('tmpdir') // using os.tmpdir() as base temporary directory
+// You can also configure the location where temporary directories should be created by calling
+const noCleanup = true; // Optionally suspends deletion policy
+const instance = tmpDir.build('/my/temp_path', noCleanup);
 
+// instance is now a new tempDir instance that you can use separately
+// whereas reimports of tempdir via require() will give you the same instance as a singleton
+// Usually the singleton is plenty, using .build() should happen very infrequently.
+```
+
+## Deletion Strategy
+Only the most three recent runs are being kept.
--- lib.js
+++ lib.js
@@ -1,40 +1,104 @@
-'use strict';
-
-const fs = require('fs');
-const os = require('os')
-const path = require('path')
+const fs = require('fs-extra');
+const os = require('os');
+const path = require('path');
+const process = require('process');
 const shortid = require('shortid');
+const Promise = require('bluebird');
+
+const isoRegexStr = /tempDir_\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/g;
+
+/* eslint-disable no-underscore-dangle */
+let _singleton = null;
 
 /**
- * TODO(cuontheinternet): Tests
- * TODO(cuontheinternet): Optional arg when requiring this
- * TODO(cuontheinternet): CI
- * TODO(cuontheinternet): Scope-limited factory thing
  * TODO(cuontheinternet): Check that func is not anonymous/stack trace in tests is nice
- * TODO(cuontheinternet): Delete behavior of pytest
- * TODO(cuontheinternet): Implement delete strategy
- * TODO(cuontheinternet): Docs
- * TODO(cuontheinternet): Manual
  * TODO(cuontheinternet): Publish
+ * Assumptions:
+ * - TempDir might be called from several processes
+ *  - TempDir might be imported multiple times
+ * - We might want to persist some temp dir
+ * Policies:
+ * - Cleanup sometimes
+ *  - Track
+ * - All
+ * 1. Create baseDir if it's not there
+ *    - Issue:
+ *    - sessionDir is supposed to be called each time we import TempDir
+ *    - delete
  */
 class TempDir {
-  constructor(baseDir) {
+  constructor(baseDir, noCleanup) {
     if (baseDir == null || baseDir === false) {
-      baseDir = os.tmpdir()
+      if (fs.existsSync(os.tmpdir())) {
+        this.baseDir = os.tmpdir();
+      } else if (fs.existsSync('/tmp/')) {
+        this.baseDir = '/tmp/';
+      } else {
+        this.baseDir = process.cwd();
+      }
+    } else {
+      this.baseDir = baseDir;
     }
+    this.noCleanup = noCleanup;
 
-    this.baseDir = baseDir;
+    const now8601 = (new Date()).toISOString();
+    const dirName = `tempDir_${now8601}`;
+    this.instanceDir = path.join(this.baseDir, dirName);
+
+    if (!noCleanup) {
+      this.deletePreviousRuns();
+    }
   }
 
-  make() {
-    const now8601 = (new Date()).toISOString();
-    const shortId = shortid.generate();
-    const tmpDir = path.join(this.baseDir, now8601 + shortId);
+  deletePreviousRuns() {
+    let allRunDirNames = fs.readdirSync(this.getBaseDir());
+    const reg = new RegExp(isoRegexStr, 'g');
+    allRunDirNames = allRunDirNames.filter(dirName => dirName.match(reg));
+    const oldestRunNames = allRunDirNames.sort().reverse();
+    const toDeleteNames = oldestRunNames.slice(2, allRunDirNames.length);
+    const delPromises = toDeleteNames.map(
+      /* eslint-disable comma-dangle */
+      dirName => fs.remove(path.join(this.getBaseDir(), dirName))
+    );
+    Promise.all(delPromises);
+  }
 
-    if (!fs.existsSync(tmpDir)) {
-      fs.mkdirSync(tmpDir);
+  getBaseDir() {
+    return this.baseDir;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  isBaseDir(candidate) {
+    return candidate.match(new RegExp(isoRegexStr, 'g'));
+  }
+
+  getSingleton() {
+    if (!_singleton) {
+      _singleton = this;
     }
-    return tmpDir;
+    return _singleton;
+  }
+
+  // seems better than to execute functions by default.
+  // also maybe an antipattern
+  /* eslint-disable class-methods-use-this */
+  build(baseDir = null, noCleanup = false) {
+    return new TempDir(baseDir, noCleanup);
+  }
+
+  provide() {
+    // TODO(cuontheinternet): Mode etc.
+    // I heard the cool kids on the block use promises these days
+    return new Promise((resolve, reject) => {
+      try {
+        const shortId = shortid.generate();
+        const tmpDir = path.join(this.instanceDir, `${shortId}`);
+        resolve(tmpDir);
+      } catch (e) {
+        reject(e);
+      }
+    }).then(tmpDir => fs.ensureDir(tmpDir).then(() => tmpDir));
   }
 }
-module.exports = TempDir;
+
+module.exports = new TempDir(os.tmpdir(), false).getSingleton();
